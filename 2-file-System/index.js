5104612e10f6b89157700a949e48cf46e7ee5936
asjad <asjad.ali5968@gmail.com>
2025-04-24 15:34:39 +0500

    add http module

--- 2-file-System/index.js
+++ 2-file-System/index.js
@@ -12,6 +12,14 @@ if (!fs.existsSync(dataFolder)) {
 
 const filepath = path.join(dataFolder, "data.txt");
 
+//Creates or overwrites a file with the given data
 fs.writeFileSync(filepath, "Hello from asjad adding data in file using code");
 
+//read data in file utf-8 mean in string format
 console.log("read content from file", fs.readFileSync(filepath, "utf-8"));
+
+//add more data in file
+fs.appendFileSync(
+  filepath,
+  "\nhallo this is second line in this specific file"
+);
